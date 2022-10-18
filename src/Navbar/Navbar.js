3401370c59d707f17e57ca73cc57c36f7a8e71ab
Yilika Loufoua <yilika.loufoua@tcu.edu>
2022-10-18 02:17:34 -0500

    Update Navbar.js

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -4,7 +4,9 @@ import logo from "../assets/logo.png";
 function Navbar() {
   return (
     <div className="navbar">
-      <img src={logo} alt="codepath-logo"/>
+      <a href="/">
+        <img src={logo} alt="codepath-logo"/>
+      </a>
       <a href="https://www.codepath.org/about">About</a>
       <a href="https://www.codepath.org/courses">Courses</a>
       <a href="https://www.codepath.org/contact">Contact</a>
